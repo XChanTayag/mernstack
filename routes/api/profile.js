826cd8c688d19fac81cd9bc83bf5379a639e4d09
XChan <xtian.tayag@gmail.com>
2023-08-17 11:30:37 +0800

    add profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../model/Profile');
+const User = require('../../model/User');
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile Route'));
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
